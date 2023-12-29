4eec4f2bce3b98a88744111d03813b59fd010f79
abrazax56 <ahmadbenirusli@gmail.com>
2023-12-29 14:32:06 +0700

    fourth-commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,4 @@
+
 export default function Banner() {
   return (
     <div className="imgbanner w-100 d-flex justify-content-center align-items-center flex-column">
@@ -5,6 +6,10 @@ export default function Banner() {
         <h1 className="judul text-warning mb-3 pb-1">Wattpad Formal</h1>
         <p className="subJudul text-white fs-5"><i className="bi bi-pen"></i> Loyalitas Tanpa Batas</p>
         <p className="subJudul text-white fs-7">04 April 2022</p>
+        <audio id="aud" loop="true" preload="true">
+          <source src="/audio/audio.m4a" type="audio/mp3"/>
+        </audio>
+        <a href="#secone" className="sosmed px-3 py-1 fs-7 text-warning" onClick={() => document.getElementById('aud').play()}>Get Started</a>
       </div>
     </div>
   )
