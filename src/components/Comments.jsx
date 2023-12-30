fa6afc14f7ac73869a7557307d4b8d74fa6a54f6
abrazax56 <ahmadbenirusli@gmail.com>
2023-12-30 20:56:54 +0700

    seven-commit

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,3 +1,4 @@
+import DataComment from './DataComment.jsx'
 function HandleClickSubmit(e) {
   e.preventDefault();
 }
@@ -7,12 +8,8 @@ export default function Comments() {
     <div className="mainSection p-5 d-flex flex-column align-items-baseline">
       <h1 className="judulkomen judulsection fs-3"><i className="bi bi-chat-dots"></i> Komentar [mendatang...]</h1>
       <div className="mt-5 p-3 jadwal w-100">
-        <div className="box-coment d-flex flex-column gap-3 p-3">
-          <div className="komentar">
-            <p className="nama-komen px-3 pb-1 pt-1 text-warning"><i className="bi bi-person"></i> Ahmad Beni Rusli</p>
-            <div><p className="text-white px-3 h-100">Wiihhh bagus banget kak</p></div>
-            <div className="px-3 d-flex justify-content-baseline"><p className="wajib fs-7 px-2"><i className="bi bi-clock"></i> 27/12/2023</p></div>
-          </div>
+        <div className="box-coment d-flex flex-column-reverse gap-3 p-3">
+          <DataComment/>
         </div>
         <form className="mt-4" onSubmit={HandleClickSubmit}>
           <label className="text-warning">Masukan nama</label>
