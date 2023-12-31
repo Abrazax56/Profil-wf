3bb74a78e39f9130f9fdac2db34714861bc26823
abrazax56 <ahmadbenirusli@gmail.com>
2023-12-31 09:30:24 +0700

    eight-commit

--- src/components/DataComment.jsx
+++ src/components/DataComment.jsx
@@ -7,34 +7,30 @@ const query = encodeURIComponent('Select *')
 const url = `${base}&sheet=${sheetName}&tq=${query}`
 
 export default function Datas() {
-  const [data, setData] = useState('');
-  const [datas, setDatas] = useState([]);
+  const [arr, setArr] = useState([])
   useEffect(() => {
     const fetchData = async () => {
       const result = await fetch(url)
       .then(response => response.text())
       .then(res => {
         const jsonData = JSON.parse(res.substring(47).slice(0, -2));
-        const newData = jsonData.table.rows
-        const komen = [...jsonData]
-        setData(newData[0].c[0].f)
-        setDatas(komen)
+        const comment = jsonData.table.rows
+        setArr([...comment])
       });
     };
     fetchData();
   }, []);
-  
   return (
     <>
-     {
-       datas.map(komen => (
-         <div className="komentar" key={komen.c[0].v}>
-           <p className="nama-komen px-3 pb-1 pt-1 text-warning"><i className="bi bi-person"></i> {komen.c[1].v}</p>
-           <div><p className="text-white px-3 h-100">{komen.c[2].v}</p></div>
-           <div className="px-3 d-flex justify-content-baseline"><p className="wajib fs-7 px-2"><i className="bi bi-clock"></i> {komen.c[0].f}</p></div>
+      { 
+        arr.map(comments => (
+         <div className="komentar" key={comments.c[2].v}>
+           <p className="nama-komen px-3 pb-1 pt-1 text-warning"><i className="bi bi-person"></i> {comments.c[1].v}</p>
+           <div><p className="text-white px-3 h-100">{comments.c[2].v}</p></div>
+           <div className="px-3 d-flex justify-content-baseline"><p className="wajib fs-7 px-2"><i className="bi bi-clock"></i> {comments.c[0].f}</p></div>
          </div>
-       ))
-     }
-   </>
+        ))
+      }
+    </>
   )
 }
