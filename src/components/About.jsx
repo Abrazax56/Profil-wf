34903b2d0a8d76a769007702c5f6fec0910e58d0
abrazax56 <ahmadbenirusli@gmail.com>
2024-01-04 13:52:36 +0700

    twenty-commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import Histories from './AboutHistories.jsx';
 
 export default function About() {
   return (
-    <div className="mainSection p-5 d-flex flex-column align-items-baseline">
+    <div className="p-5 d-flex flex-column align-items-baseline mainSection">
       <h1 className="judulsection text-white fs-3"><i className="bi bi-book"></i> Tentang <span className="text-warning">Wattpad Formal</span></h1>
       <Introduce/>
       <Histories/>
